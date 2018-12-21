c21bacfaa04ef6e9fd9d59366214f8d541e27aab
MarSchlaefer <mschlaefer00@gmail.com>
2018-12-21 17:41:16 -0500

    Added footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import twitter from '../images/iconmonstr-twitter-5.svg'
+import facebook from '../images/iconmonstr-facebook-5.svg'
+import instagram from '../images/iconmonstr-instagram-11.svg'
+import linkedin from '../images/iconmonstr-linkedin-3.svg'
 
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-
+        <div className="social">
+          <img src={ facebook } alt=""/>
+          <img src={ twitter } alt=""/>
+          <img src={ instagram } alt=""/>
+          <img src={ linkedin } alt=""/>
+        </div>
       </div>
     );
   }
