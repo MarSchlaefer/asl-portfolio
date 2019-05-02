d1a17f00f89ca8306abe020d2a9c201955980421
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-02 15:43:22 -0400

    Added state for page change.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,46 @@ import Footer from './components/footer'
 import Content from './components/content'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      page: "about"
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Content />
+        <Header handleClick={this.handleClick}/>
+        <Content page={this.state.page}/>
         <Footer />
       </div>
     );
+  }
+
+  handleClick = (pageClick) => {
+    if (pageClick === "about") {
+      return this.setState({
+        page: "about"
+      })
+    } else if (pageClick === "experience") {
+      return this.setState({
+        page: "experience"
+      })
+    } else if (pageClick === "samples") {
+      return this.setState({
+        page: "samples"
+      })
+    } else if (pageClick === "availability") {
+      return this.setState({
+        page: "availability"
+      })
+    } else if (pageClick === "contact") {
+      return this.setState({
+        page: "contact"
+      })
+    }
   }
 }
 
