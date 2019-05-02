4fb8e646a4e8bf0711712022f85bf5c8ba3caaea
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-02 15:43:47 -0400

    Added extra info pages.

---
+++ src/components/availability.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Availability extends Component {
+  render() {
+    return (
+      <div className="Availability">
+        <h1>Availability</h1>
+      </div>
+    );
+  }
+}
+
+export default Availability;
---
+++ src/components/experience.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+
+class Experience extends Component {
+  render() {
+    return (
+      <div className="experience">
+        <h1>EXPERIENCE</h1>
+        <div className="col-container">
+          <div className="col-1">
+            <h2>Education</h2>
+            <br />
+            <h3>Interpreter Preparation, Associates Degree, Community College of Baltimore County, December 2016.</h3>
+            <h3>Deaf Studies, Bachelor of Science, Towson University, Towson, May 2010.</h3>
+            <br />
+          </div>
+          <div className="col-2">
+            <h2>Course Work</h2>
+            <br />
+            <h3>ASL 1-5</h3>
+            <h3>Introduction to Deafblind</h3>
+            <h3>Linguistics of ASL</h3>
+            <h3>Consecutive Interpreting</h3>
+            <h3>Interactive Interpreting</h3>
+            <h3>Transliterating</h3>
+            <h3>Simultaneous Interpreting</h3>
+            <h3>Deaf Culture</h3>
+            <h3>Deaf Literature</h3>
+            <h3>Deaf History</h3>
+          </div>
+          <div className="col-3">
+            <h2>Skills</h2>
+            <br />
+            <h3>Time Management:&nbsp; Organized a team to assemble over 1,400 conference materials, 2013.</h3>
+            <h3>Social Media:&nbsp; Organized several Deaf Events for CCBC ASL Club, 2015.</h3>
+            <h3>Public Speaking: &nbsp;Recognized at State of Iowa – All State Speech Competition, 1995.</h3>
+          </div>
+        </div>
+        <br />
+        <h2>Full Resume</h2>
+        <br />
+        <span><h2>For full resume click: </h2></span> <span><a href="https://drive.google.com/file/d/0B-gD9JdC00ZLSThaakhIVXdnb1U/view?usp=sharing">BRONWYN'S RESUME’</a></span>
+      </div>
+    );
+  }
+}
+
+export default Experience;
