3e06f4bada942850927c5cfb93f944ac90678666
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-03 11:21:41 -0400

    Removed state and click handler.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Footer from './components/footer'
 import Content from './components/content'
 import SideBar from './components/sideBar'
 
 class App extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      page: "about"
-    }
-  }
-
   render() {
     return (
       <div className="App">
         <SideBar handleClick={this.handleClick}/>
+        <div className="spacing"></div>
         <Content page={this.state.page}/>
       </div>
     );
-  }
-
-  handleClick = (pageClick) => {
-    if (pageClick === "about") {
-      return this.setState({
-        page: "about"
-      })
-    } else if (pageClick === "experience") {
-      return this.setState({
-        page: "experience"
-      })
-    } else if (pageClick === "samples") {
-      return this.setState({
-        page: "samples"
-      })
-    } else if (pageClick === "availability") {
-      return this.setState({
-        page: "availability"
-      })
-    } else if (pageClick === "contact") {
-      return this.setState({
-        page: "contact"
-      })
-    }
   }
 }
 
