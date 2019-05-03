536e1cdc0cea5e16f818f733f0185f01d20f0b58
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-03 11:26:09 -0400

    Imported logo.

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Menu from './menu'
+import logo from '../images/logo.png'
 
 export default class SideBar extends Component {
   render() {
     return (
       <div className="side-bar">
+        <img src={logo} alt="Logo"/>
         <Menu handleClick={this.props.handleClick}/>
       </div>
     )
