39ec6234bbc31654cf8e7e7353e34dc596722c10
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-06 14:29:54 -0400

    Switched intro video.

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ class About extends Component {
         <h5>“It all started when Brian walked into the wrong classroom and it changed his life forever.&nbsp; Brian compares it to the a secret passage way of pure fate.”</h5>
         <p>On that day Brian began a journey that is still continuing today as a sign language interpreter.&nbsp; He enjoys knowing that as an interpreter two people are able to make a connection and learn from each other.</p>
         <br />
-        <ReactPlayer url='https://www.youtube.com/watch?v=tLNm9JkARgE' />
+        <ReactPlayer url='https://youtu.be/qmLfxEcsa0g' />
       </div>
     );
   }
