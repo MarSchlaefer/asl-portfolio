53f84632bc4e60a4938133563b34d491f0a2d654
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-07 14:54:48 -0400

    Imported axios. Removed semantic imports.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
-// import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
 
 
 class Contact extends Component {
