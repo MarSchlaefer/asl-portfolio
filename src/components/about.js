573c7ff7f2fb81d0f8e1759c65cc81ff72bef00d
MarSchlaefer <mschlaefer00@gmail.com>
2019-05-08 17:43:11 -0400

    Rmoved placeholder text.

--- src/components/about.js
+++ src/components/about.js
@@ -9,8 +9,6 @@ class About extends Component {
         <div className="page-title">
           <h1>About</h1>
         </div>
-        <h5>“It all started when Brian walked into the wrong classroom and it changed his life forever.&nbsp; Brian compares it to the a secret passage way of pure fate.”</h5>
-        <p>On that day Brian began a journey that is still continuing today as a sign language interpreter.&nbsp; He enjoys knowing that as an interpreter two people are able to make a connection and learn from each other.</p>
         <br />
         <ReactPlayer url='https://youtu.be/qmLfxEcsa0g' />
       </div>
